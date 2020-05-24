f97ef36c2847f89145bcd6906fe7264a89ff54f2
evaldas23 <evaldas.cerniauskas@gmail.com>
2020-05-24 23:30:53 +0300

    Integrating dates

--- notes-functions.js
+++ notes-functions.js
@@ -52,8 +52,24 @@ const generateNoteDOM = function (note) {
     return noteEl
 }
 
+// Sort your notes by one of three ways
+const sortNotes = function (notes, sortBy) {
+    if (sortBy === 'byEdited') {
+        return notes.sort(function (a, b){
+            if (a.updatedAt > b.updatedAt){
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } return notes
+} 
+
 // Render application notes
 const renderNotes = function (notes, filters) {
+    notes = sortNotes(notes, filters.sortBy)
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
